cc49267a70786e2ecef0b8cc81caedc7ad0e2551
AI-Stratov <workistratov@gmail.com>
2024-04-09 12:50:07 +0300

    v 0.0.2 & actions

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import (
     find_packages,
 )
 
-__version__: str = "0.0.1"
+__version__: str = "0.0.2"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="lowmq-client",
     version=__version__,
+    description="Python asynchronous client for interacting with LowMQ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
